1c09267ab1b60824ac369692e7ed167de08c05c7
Mkilord <mkilord6666@gmail.com>
2024-07-19 11:43:00 +0300

    add fragments and design changes

--- src/main/java/ru/mkilord/tacos/controller/WebConfig.java
+++ src/main/java/ru/mkilord/tacos/controller/WebConfig.java
@@ -9,6 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
+        registry.addViewController("/about_us");
         registry.addViewController("/order_created").setViewName("order_created");
     }
 }
